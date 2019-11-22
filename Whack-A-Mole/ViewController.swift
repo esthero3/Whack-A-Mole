522517fc1c69aaccd142553004bd4a67cdcb52ee
Esther <esthero3@learn.cssd.ab.ca>
2019-11-22 11:21:25 -0700

    Initial Commit

---
+++ Whack-A-Mole/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whack-A-Mole
+//
+//  Created by Esther Osammor on 2019-11-22.
+//  Copyright © 2019 Esther Osammor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
